029061dd8f1181579ad5fd53e84805e1d911b057
asathkumara <sathkumaraasel@gmail.com>
2021-11-22 10:24:06 -0800

    Documented

--- src/main/client/src/components/Footer.js
+++ src/main/client/src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Footer.css";
 
+/**
+ * Container for custom Footer
+ * @param children The child components
+ * @returns {JSX.Element}
+ */
 const Footer = ({ children }) => {
     return <footer>{children}</footer>
 }
--- src/main/client/src/components/Logo.js
+++ src/main/client/src/components/Logo.js
@@ -2,6 +2,10 @@ import React from "react";
 import logo from "../resources/images/ncrp-logo.png"
 import "./Logo.css"
 
+/**
+ * Container for site logo
+ * @returns {JSX.Element}
+ */
 const Logo = () => {
     return <img src={logo} className="app-logo" alt="app-logo"/>
 }
