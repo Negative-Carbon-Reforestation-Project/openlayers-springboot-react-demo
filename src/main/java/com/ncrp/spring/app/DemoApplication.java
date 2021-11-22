e3e96e49e51e31d2f52e6fe8ee9801ec0cc4fdca
asathkumara <sathkumaraasel@gmail.com>
2021-11-21 17:18:19 -0800

    Changed package name

--- src/main/java/com/ncrp/spring/app/DemoApplication.java
+++ src/main/java/com/ncrp/spring/app/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.ncrp.demo;
+package com.ncrp.spring.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ncrp/spring/app/DemoApplicationTests.java
+++ src/test/java/com/ncrp/spring/app/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ncrp.demo;
+package com.ncrp.spring.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
