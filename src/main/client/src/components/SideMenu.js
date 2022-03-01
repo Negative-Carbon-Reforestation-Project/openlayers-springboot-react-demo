e8078587eed7b317778a7f95aaf4d5d11722bcaf
asathkumara <sathkumaraasel@gmail.com>
2022-02-28 16:04:48 -0800

    Implemented basic layer toggling - no styles

--- src/main/client/src/components/SideMenu.js
+++ src/main/client/src/components/SideMenu.js
@@ -1,6 +1,7 @@
-import React, {useRef} from "react";
+import React, {useContext, useRef} from "react";
 import {Drawer} from "@mui/material";
 import SideMenuContext from "./SideMenuContext";
+import MapContext from "./Map/MapContext";
 
 /**
  * Container for side menu
@@ -8,11 +9,20 @@ import SideMenuContext from "./SideMenuContext";
 const SideMenu = ({children}) => {
     const layerMenuRef = useRef();
     const sideMenuRef = useRef();
+    const {map} = useContext(MapContext);
 
     const hideSideMenu = () => {
         layerMenuRef.current.style.display = "none";
         sideMenuRef.current.style.display = "none";
     };
+
+    const toggleLayer = (event) => {
+        let layerIndex = Number(event.target.getAttribute("data-value"));
+        let layers = map.getLayers();
+
+        let isLayerVisible = layers.item(layerIndex).getVisible();
+        layers.item(layerIndex).setVisible(!isLayerVisible);
+    }
 
     return (
         <SideMenuContext.Provider value={{sideMenuRef, layerMenuRef}}>
@@ -21,6 +31,21 @@ const SideMenu = ({children}) => {
                     <section className="layer-menu-header">
                         <h1 className="layer-menu-heading">Layers</h1>
                         <button className="layer-menu-exit" onClick={() => hideSideMenu()}>X</button>
+                    </section>
+
+                    <section className="layer-menu-content">
+                        <article className="layer-item">
+                            <h2 className="layer-item-heading" data-value={0} onClick={(event) => toggleLayer(event)}>Layer 0</h2>
+                        </article>
+                        <article className="layer-item">
+                            <h2 className="layer-item-heading" data-value={1} onClick={(event) => toggleLayer(event)}>Layer 1</h2>
+                        </article>
+                        <article className="layer-item">
+                            <h2 className="layer-item-heading" data-value={2} onClick={(event) => toggleLayer(event)}>Layer 2</h2>
+                        </article>
+                        <article className="layer-item">
+                            <h2 className="layer-item-heading" data-value={3} onClick={(event) => toggleLayer(event)}>Layer 3</h2>
+                        </article>
                     </section>
                 </div>
 
