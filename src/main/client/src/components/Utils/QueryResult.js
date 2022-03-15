eb0b01ec95570bc7ecfd5da0e250c188a1519401
asathkumara <sathkumaraasel@gmail.com>
2022-03-14 19:41:59 -0700

    Updated popup and query search result

--- src/main/client/src/components/Utils/QueryResult.js
+++ src/main/client/src/components/Utils/QueryResult.js
@@ -6,10 +6,19 @@ const QueryResult = ({data, coordinate}) => {
     const longLatInfo = toStringHDMS(coordinate);
     const longLatDisplay = `${String.fromCodePoint("0x1F4CD")} ${longLatInfo}`;
 
+    const calculateReforestationOpportunity = () => {
+        if (data.wa_total_reforestation_opportunity)
+        {
+            return (data.wa_total_reforestation_opportunity * 100).toFixed(2);
+        }
+
+        return 0;
+    }
+
     return (
         <div className="query-content">
             <p className="coordinates">{longLatDisplay}</p>
-            <p className="query-result">{data}</p>
+            <p className="query-result">Reforestation Opportunity: {calculateReforestationOpportunity()}%</p>
         </div>
     );
 }
