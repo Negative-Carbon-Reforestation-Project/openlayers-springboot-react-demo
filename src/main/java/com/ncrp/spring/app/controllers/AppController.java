2f22eff253d2303945cdbc997ee297b715869d19
asathkumara <28933557+asathkumara@users.noreply.github.com>
2022-04-12 14:16:14 -0700

    Added Spring Boot support for React-Router and added improved handler stub for future editing

---
+++ src/main/java/com/ncrp/spring/app/controllers/AppController.java
@@ -0,0 +1,32 @@
+package com.ncrp.spring.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+/**
+ * The controller for the app.
+ *
+ * @remarks: Needed for forward compatiblity with React-Router.
+ */
+public class AppController
+{
+    @RequestMapping(
+        value = {
+                "/",
+                "/index",
+                "/mission",
+                "/accessibility",
+                "/terms",
+                "/privacy",
+                "/maps"
+        }
+    )
+    /**
+     * Returns the index page when requests are sent to the routes specified in the request mapping
+     */
+    public String getIndex()
+    {
+        return "/index.html";
+    }
+}
