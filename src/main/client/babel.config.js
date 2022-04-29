ea092d92de43c1216117cfaf6c33b8b87149954b
asathkumara <28933557+asathkumara@users.noreply.github.com>
2022-04-29 16:21:41 -0700

    Added unit test stubs for Spring Boot, still some issues with React unit test stubs

--- src/main/client/babel.config.js
+++ src/main/client/babel.config.js
@@ -1,9 +1,7 @@
-// Babel config file. Required due to manual jest use instead of via react-scripts test
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    env: {
-        test: {
-            plugins: ["@babel/plugin-transform-runtime"]
-        }
-    }
-};
+// // Babel config file. Required due to manual jest use instead of via react-scripts test
+// module.exports = {
+//     presets: [
+//         '@babel/preset-env',
+//         ['@babel/preset-react', {runtime: 'automatic'}],
+//     ],
+// };
