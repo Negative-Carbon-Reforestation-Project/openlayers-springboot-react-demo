937f2b0966ef6549692995f40b929d2f089c7eb0
Matthew Tkachuk <quicktkachuk@gmail.com>
2022-05-03 18:49:27 -0700

    Adding framework for geoencoding (currently pointed at google, but this is likely to change)

---
+++ src/main/java/com/ncrp/spring/app/controllers/GoogleGeoEncodingController.java
@@ -0,0 +1,2 @@
+package com.ncrp.spring.app.controllers;public class GoogleGeoEncodingController {
+}
---
+++ src/main/java/com/ncrp/spring/app/services/GoogleGeoEncodingService.java
@@ -0,0 +1,2 @@
+package com.ncrp.spring.app.services;public class GoogleGeoEncodingService {
+}
