b412ca04a8b0f76ad80423a65465a20ef3198191
Matthew Tkachuk <quicktkachuk@gmail.com>
2022-05-10 19:21:12 -0700

    setting up framework for google geo encoding

--- src/main/java/com/ncrp/spring/app/controllers/GoogleGeoEncodingController.java
+++ src/main/java/com/ncrp/spring/app/controllers/GoogleGeoEncodingController.java
@@ -1,2 +1,26 @@
-package com.ncrp.spring.app.controllers;public class GoogleGeoEncodingController {
+package com.ncrp.spring.app.controllers;
+
+import com.ncrp.spring.app.services.GoogleGeoEncodingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class GoogleGeoEncodingController {
+    private final GoogleGeoEncodingService geoEncodingService;
+
+    @Autowired
+    public GoogleGeoEncodingController(GoogleGeoEncodingService geoEncodingService)
+    {
+
+        this.geoEncodingService = geoEncodingService;
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "http://localhost:3000")
+    public String getAddressQuery(@RequestParam("address") String address)
+    {
+        String coords = geoEncodingService.getCoords(address);
+        return coords;
+    }
 }
--- src/main/java/com/ncrp/spring/app/services/GoogleGeoEncodingService.java
+++ src/main/java/com/ncrp/spring/app/services/GoogleGeoEncodingService.java
@@ -1,2 +1,60 @@
-package com.ncrp.spring.app.services;public class GoogleGeoEncodingService {
+package com.ncrp.spring.app.services;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+// Should take in an address from app and hook into Google Geo Encoding API
+    // https://developers.google.com/maps/documentation/geocoding/start
+public class GoogleGeoEncodingService {
+    private final String baseURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+    private String address;
+//    private URLConnection urlConnection;
+
+    public GoogleGeoEncodingService()
+    {
+
+    }
+
+    public String getCoords(String address)
+    {
+        try
+        {
+            URL geoEncodURL = new URL(getURL(address));
+        }
+        catch (MalformedURLException error)
+        {
+            System.out.println("Forming the URL failed");
+            //TODO: I want this to return an error informing user that they need to re-enter the address or contact us
+            return null;
+        }
+        catch(IOException error)
+        {
+            System.out.println("Opening the connection failed");
+            //TODO: Should return a connection error notice to user. They should either wait and try again or contact us
+            return null;
+        }
+        return null;
+    }
+
+    private String getURL(String address)
+    {
+
+        String[] splitAddress = address.split(" ");
+        String finalURL = baseURL + splitAddress[0];
+        for(int i = 1; i < splitAddress.length; i++)
+        {
+            finalURL += "+" + splitAddress[i];
+        }
+
+        return finalURL;
+    }
+
+    public static void main(String[] args)
+    {
+        GoogleGeoEncodingService thing = new GoogleGeoEncodingService();
+        System.out.println(thing.getCoords("1600 Amphitheatre Parkway, Mountain View, CA"));
+    }
+
 }
