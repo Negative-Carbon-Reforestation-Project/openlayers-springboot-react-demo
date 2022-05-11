0998ec29c760c30ff82561e459f9e5513b209eab
asathkumara <28933557+asathkumara@users.noreply.github.com>
2022-05-10 21:20:41 -0700

    Fixing folder tracking pt. 1

--- src/main/client/src/components/Map/MapContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-/**
- * React context for OpenLayer Map.
- * @type {React.Context<unknown>}
- */
-const MapContext = new React.createContext();
-
-export default MapContext;
