03c7b9caae7f45fc0b18f3cc34cc47f4627affb8
asathkumara <28933557+asathkumara@users.noreply.github.com>
2022-05-18 13:02:39 -0700

    Added functionality to zoom in / out

--- src/main/client/src/components/controls/CameraControl.js
+++ src/main/client/src/components/controls/CameraControl.js
@@ -1,16 +1,26 @@
+import {useSelector} from "react-redux";
+
 /**
  * Container for the Camera controls
  * @returns {JSX.Element}
  */
 const CameraControl = () => {
+    const map = useSelector((state) => state.maps.value.map);
+
+    const zoom = (modifier=0.5) => {
+        let view = map.getView();
+        let currentZoomLevel = view.getZoom();
+        view.setZoom(currentZoomLevel + modifier);
+    }
+
     return (
         <>
             <div className="zoom-controls" tabIndex={0} aria-label="Zoom in and out controls">
-                <button className="zoom-in-control" aria-label="Zoom in">
+                <button className="zoom-in-control" aria-label="Zoom in" onClick={() => zoom()} title="Zoom in">
                     +
                 </button>
 
-                <button className="zoom-out-control" aria-label="Zoom out">
+                <button className="zoom-out-control" aria-label="Zoom out" onClick={() => zoom(-0.5)} title="Zoom out">
                     -
                 </button>
             </div>
